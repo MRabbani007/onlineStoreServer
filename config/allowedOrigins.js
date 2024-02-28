c7d7a2ece943104419a8dd19bb944d798624cd9a
Mohamad Rabbani <mrabbani@outlook.com>
2024-02-29 00:58:03 +0500

    Add auth

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 // Cross Origin Resource Sharing
 const allowedOrigins = [
   "https://mrabbani007.github.io",
-  "https://mrabbani007.github.io/todolist",
+  "https://mrabbani007.github.io/onlineStore",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
   "http://localhost:5173",
